6af37cbf5d5c57a44887234a1a47383b430d2467
Hilal AVCU <90915988+Hilalavcu@users.noreply.github.com>
2024-09-06 19:54:45 +0300

    Add files via upload

---
+++ CalismaYapisi/CalismaYapisi/SonucEkraniVC.swift
@@ -0,0 +1,27 @@
+//
+//  SonucEkraniVC.swift
+//  CalismaYapisi
+//
+//  Created by Hilal AVCU on 23.08.2024.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonKapat(_ sender: Any) {
+        
+        self.dismiss(animated: true) //bulundugu sayfayi kapatir
+        
+    }
+    
+    
+    
+}
